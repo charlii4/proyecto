0dcdac1e6d7592d172d86b0038f14c45c7f4d6e3
charliVB <charliVB@charliVB-PC>
2018-05-15 17:14:33 +0200

    inicio

---
+++ src/proyecto/Proyecto.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto;
+
+/**
+ *
+ * @author charliVB
+ */
+public class Proyecto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+    }
+    
+}
